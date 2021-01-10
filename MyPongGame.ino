59eb4f292edcf035f78ddaec1a0771441a297ead
Roman Alexandru <romanalexandru123@yahoo.com>
2021-01-10 19:52:33 +0200

    Arduino Pong Game

--- MyPongGame.ino
+++ MyPongGame.ino
@@ -1,4 +1,3 @@
-#include <U8glib.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -11,8 +10,11 @@
 #define DOWN_BUTTON_P1 6
 #define UP_BUTTON_P2 5
 #define DOWN_BUTTON_P2 4
+#define AI_BUTTON 3
+#define AI_LED 2
+#define PAUSE_BUTTON 1
 
-// Declaration for SSD1306 display connected using software SPI (default case):
+// SPI 
 #define OLED_MOSI   9 //d1
 #define OLED_CLK   10 //d0
 #define OLED_DC    12
@@ -20,30 +22,22 @@
 #define OLED_RESET 13
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-  
-U8GLIB_SSD1306_128X64 u8g(6, 2, U8G_PIN_NONE, 4 , 5 );
+
 #define RED 0xF800
 
-/**
- * Made with Marlin Bitmap Converter
- * https://marlinfw.org/tools/u8glib/converter.html
- * This bitmap from the file 'arduinopongTExt.jpg'
- */
+
 //------------------------------------------ GAME CONSTANTS
-const int MAX_SCORE = 10;
-const int PADDLE_SPEED = 10;
+const int MAX_SCORE = 5;
 const int PADDLE_SIZE = 18;
-const int BALL_SPEED = 10;
 
 //------------------------------------------- GAME VARIABLES
 //----------------------------------players
-int player1Score, player2Score = 0; 
+int player1Score = 0, player2Score = 0; 
 
 //----------------------------------ball
 unsigned int ballX=64, ballY=32; 
 unsigned int ballDirX=1, ballDirY=1; 
 
-//long ballUpdate;
 
 //----------------------------------paddles
 unsigned int paddle2X = 8;
@@ -51,7 +45,6 @@ unsigned int paddle2Y = 16;
 
 unsigned int paddle1X = 120;
 unsigned int paddle1Y = 16;
-//long paddleUpdate;
 
 //----------------------------------other
 boolean GameRunning = true;
@@ -67,7 +60,7 @@ void welcome()                                                        //####
   display.setTextWrap(false);
   display.print("WELCOME TO");
 
-  display.setCursor(21,20);
+  display.setCursor(21,22);
   display.setTextColor(WHITE);
   display.setTextSize(2);
   display.setTextWrap(false);
@@ -92,26 +85,37 @@ void pressButtonToStart()                                                       
   display.print("press button to enter");
   display.display();                                                        
   display.clearDisplay();
-  delay(2000);
+  delay(200);
   
 }
-void printNumber(int nr, int x, int y)                                                      //####
+void printNumberUtil(int nr, int x, int y, int textSize)                                                      //####
 {
   display.setCursor(x,y);
   display.setTextColor(WHITE);
-  display.setTextSize(1);
+  display.setTextSize(textSize);
   display.setTextWrap(false);
   display.print(nr);                                                       
 }
-void printBallCoords()                                                      //####
+
+void printStringUtil(String str, int x, int y, int textSize, uint16_t color)                                                      //####
 {
-  display.setCursor(5, 2);
+  display.setCursor(x,y);
+  display.setTextColor(color);
+  display.setTextSize(textSize);
+  display.setTextWrap(false);
+  display.print(str);                                                       
+}
+void printIntermScore()                                                      //####
+{
+  display.setCursor(5+ 47, 2);
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setTextWrap(false);
-  display.print(ballX);  
-  display.setCursor(32, 2);  
-  display.print(ballY);                                                     
+  display.print(player2Score); 
+  display.setCursor(12+ 47, 2);
+  display.print("-");  
+  display.setCursor(20 + 47, 2);  
+  display.print(player1Score);                                                     
 }
 
 
@@ -122,12 +126,17 @@ void setup()
   pinMode(DOWN_BUTTON_P1, INPUT_PULLUP); 
   pinMode(UP_BUTTON_P2, INPUT_PULLUP);
   pinMode(DOWN_BUTTON_P2, INPUT_PULLUP); 
-
+  pinMode(AI_BUTTON, INPUT_PULLUP);
+  pinMode(AI_LED, INPUT_PULLUP);
+  pinMode(PAUSE_BUTTON, INPUT_PULLUP);
+  digitalWrite(AI_LED, LOW);
+  
   Serial.begin(9600);
   if(!display.begin(SSD1306_SWITCHCAPVCC)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  
   display.display();
   display.clearDisplay();
   delay(1000); // Pause for 1 sec
@@ -135,10 +144,10 @@ void setup()
   pressButtonToStart();
   while(buttonsUnpressed())  
   {
-    //Serial.println(DOWN_BUTTON_P1);
+
     delay(100);
   }
-    unsigned long start = millis();
+    
 
   delay(500);
   
@@ -146,8 +155,6 @@ void setup()
   drawScreenMargins();
   display.display();
 
-  ballX = random(30, 70);
-  ballY = random(20, 30); 
   
 }
 //--------------------------------------------------    LOOP    -------------------------------------------------- //
@@ -158,26 +165,55 @@ void drawScreenMargins(){
   display.drawRect(0,0,128,64, WHITE);
 }
 
+bool AI = false;
+bool ledBool = false;
+bool lastLedBool = false;
+bool pause = false;
+
 void loop()
 {
-  //delay(100);
-//  display.clearDisplay();
 
-  static bool P1_UP = false;
-  static bool P1_DOWN = false;
-  static bool P2_UP = false;
-  static bool P2_DOWN = false;
+  bool P1_UP = false;
+  bool P1_DOWN = false;
+  bool P2_UP = false;
+  bool P2_DOWN = false;
+
 
   P1_UP |= (digitalRead(UP_BUTTON_P1) == LOW);
   P1_DOWN |= (digitalRead(DOWN_BUTTON_P1) == LOW);
   P2_UP |= (digitalRead(UP_BUTTON_P2) == LOW);
   P2_DOWN |= (digitalRead(DOWN_BUTTON_P2) == LOW);
+  ledBool |= (digitalRead(AI_BUTTON) == LOW);
+  pause |= (digitalRead(PAUSE_BUTTON) == LOW);
+
+  while(digitalRead(PAUSE_BUTTON) == LOW){delay(30);}
   
-  drawScreenMargins();
+  while(pause){
+    delay(100);
+    if(digitalRead(PAUSE_BUTTON) == LOW){
+      pause = false; while(digitalRead(PAUSE_BUTTON) == LOW){delay(30);}
+    }
+      
+  }
+  
+  if(ledBool == true && lastLedBool == false){
+    AI = true;
+    digitalWrite(AI_LED, HIGH);
+    lastLedBool = true;
+    ledBool = false;
+  }
+  if(P1_UP ||P1_DOWN ||P2_UP || P2_DOWN){
+    AI = false;
+    digitalWrite(AI_LED, LOW);
+    lastLedBool = false;
+    ledBool = false;
+  }
+  
+  
   if(ResetBall) //when game starts
   {
-    ballX = random(30, 70);
-    ballY = random(20, 30); 
+    ballX = random(60, 68);
+    ballY = 32;
 
     do{ballDirX = random(-1,2);}
     while(ballDirX == 0);
@@ -192,31 +228,29 @@ void loop()
     int updateX = ballX + ballDirX;
     int updateY = ballY + ballDirY;
 
-    printBallCoords();
+    printIntermScore();
+    drawScreenMargins();
 
-    //printNumber(ballDirX, 56, 26);
-    //printNumber(ballDirY, 64, 26);
     
-    //-----------varificam daca mingea se loveste de peretele vericat al playerului 2(stanga)
+    //-----------varificam daca mingea se loveste de peretele verical al playerului 2(stanga)
     if(updateX == 0)
     {
       player1Score++;
       if(player1Score < MAX_SCORE)  
-        showScore();
+        showScore(2);
       else endGame();    
     }
-    //-----------varificam daca mingea se loveste de peretele vericat al playerului 1 (dreapta)
+    //-----------varificam daca mingea se loveste de peretele verical al playerului 1 (dreapta)
     if(updateX == 128)
     {
       player2Score++;
       if(player2Score < MAX_SCORE)  
-        showScore();
+        showScore(1);
       else endGame();
     }
     //-------------------------hitting the horizontal walls
     if(updateY == 2 || updateY == 62 ){
       ballDirY *= -1;
-      printNumber(ballDirY, 108, 2);
     }
     //----------------------------------hitting the paddle 1
     if(updateX == paddle1X && updateY >=paddle1Y && updateY <= paddle1Y + PADDLE_SIZE ){
@@ -230,19 +264,18 @@ void loop()
     display.drawPixel(ballX, ballY, WHITE);
     display.drawPixel(updateX, updateY, WHITE);
 
-    //ballUpdate += BALL_SPEED;
     ballX = updateX;
     ballY = updateY;
   }
 
-  if(GameRunning) //paddle movement
+  if(GameRunning && !AI) //paddle movement
   {
-    int paddle1Uppery = paddle1Y + PADDLE_SIZE, paddle2Uppery = paddle2Y + PADDLE_SIZE;
-    //paddleUpdate += PADDLE_SIZE;
+    int paddle1UpperY = paddle1Y + PADDLE_SIZE, paddle2UpperY = paddle2Y + PADDLE_SIZE;
+
     display.drawFastVLine(paddle1X, paddle1Y, PADDLE_SIZE, WHITE);
     display.drawFastVLine(paddle2X, paddle2Y, PADDLE_SIZE, WHITE);
 
-    if(paddle1Uppery < 61){
+    if(paddle1UpperY < 61){
       if(P1_DOWN) 
         paddle1Y++;
     }
@@ -250,19 +283,64 @@ void loop()
       if(P1_UP) 
         paddle1Y--;
     }
-    if(paddle2Uppery < 61){
-      if(P2_UP) 
-        paddle2Y--;
-    }
-    if(paddle2Y > 1){
+    if(paddle2UpperY < 61){
       if(P2_DOWN) 
         paddle2Y++;
+    }
+    if(paddle2Y > 3){
+      if(P2_UP) 
+        paddle2Y--;
     }
 
     P1_UP = false;
     P1_DOWN = false;
     P2_UP = false;
     P2_DOWN = false;
+  }
+  else if(GameRunning && AI){
+    int paddle1UpperY = paddle1Y + PADDLE_SIZE, paddle2UpperY = paddle2Y + PADDLE_SIZE;
+    display.drawFastVLine(paddle1X, paddle1Y, PADDLE_SIZE, WHITE);
+    display.drawFastVLine(paddle2X, paddle2Y, PADDLE_SIZE, WHITE);
+
+    bool AI1_DOWN = false;
+    bool AI1_UP = false;
+    bool AI2_DOWN = false;
+    bool AI2_UP = false;
+
+    if( ballX > (SCREEN_WIDTH - 10) / 2){
+      if((paddle1UpperY + paddle1Y)/2 > ballY){
+        AI1_UP = true;
+      }
+      if((paddle1UpperY + paddle1Y)/2 < ballY){
+        AI1_DOWN = true;
+      }
+    }
+    else if( ballX < (SCREEN_WIDTH + 10) / 2){
+      if((paddle2UpperY + paddle2Y)/2 > ballY){
+        AI2_UP = true;
+      }
+      if((paddle2UpperY + paddle2Y)/2 < ballY){
+        AI2_DOWN = true;
+      }
+    }
+
+    if(paddle1UpperY < 61){
+      if(AI1_DOWN) 
+        paddle1Y++;
+    }
+    if(paddle1Y > 3){
+      if(AI1_UP) 
+        paddle1Y--;
+    }
+    if(paddle2UpperY < 61){
+      if(AI2_DOWN) 
+        paddle2Y++;
+    }
+    if(paddle2Y > 3){
+      if(AI2_UP) 
+        paddle2Y--;
+    }
+    
   }
   display.display();
   display.clearDisplay();
@@ -271,23 +349,135 @@ void loop()
 //--------------------------------------- CONTROLLER FUNCTIONS
 boolean buttonsUnpressed()
 {
-  if(digitalRead(DOWN_BUTTON_P1) == HIGH && digitalRead(UP_BUTTON_P1) == HIGH)
+  if(digitalRead(DOWN_BUTTON_P1) == HIGH && digitalRead(UP_BUTTON_P1) == HIGH && digitalRead(DOWN_BUTTON_P2) == HIGH && digitalRead(UP_BUTTON_P2) == HIGH)
   return true;
   else return false;
 }
 //--------------------------------------- GAME FUNCTIONS
-void showScore()
+bool blinkWall = false;
+
+void showScore(int wall)
 {
-  
+  ResetBall = true;
+  display.clearDisplay();
+  display.display();
+
+  for(int i=0;i<20;i++){
+    display.clearDisplay();
+    //----------------------show score
+
+    printNumberUtil(player2Score, 51, 26, 2);
+    printStringUtil("-",61, 26, 2, WHITE);
+    printNumberUtil(player1Score, 72, 26, 2);
+
+    //----------------------
+    delay(50);
+    if(blinkWall){
+      if(wall == 1)
+        display.drawFastVLine(127, 0 , 63, WHITE);
+      else if(wall == 2)
+        display.drawFastVLine(0, 0 , 63, WHITE);
+      blinkWall = false;
+    }
+    else{
+      if(wall == 1)
+        display.drawFastVLine(127, 0 , 63, BLACK);
+      else if(wall == 2)
+        display.drawFastVLine(0, 0 , 63, BLACK);
+      blinkWall = true;
+    }
+    display.display();
+  }
+
+  blinkWall = false;
+}
+
+void printWinner(){
+  if(player1Score > player2Score){
+    delay(500);
+    printStringUtil("PLAYER 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("LAYER 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("AYER 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("YER 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("ER 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("R 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil(" 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil(" WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("ON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("N", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("", 30, 20, 1, WHITE );
+    delay(200);display.display();
+  }
+  else if(player1Score < player2Score){
+    delay(500);
+    printStringUtil("PLAYER 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("LAYER 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("AYER 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("YER 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("ER 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("R 1 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil(" 2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("2 WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil(" WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("WON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("ON", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("N", 30, 20, 1, WHITE );
+    delay(200);display.display();display.clearDisplay();
+    printStringUtil("", 30, 20, 1, WHITE );
+    delay(200);display.display();
+  }
 }
 void endGame()
 {
-  display.setCursor(10,20);
-  display.setTextColor(WHITE);
-  display.setTextSize(2);
-  display.setTextWrap(false);
-  display.print("GAME  OVER");
-  display.display();                                                        
+  
+  blinkWall = false;
+
+  for(int i=0;i<20;i++){
+    display.clearDisplay();
+    delay(50);
+    if(blinkWall){
+       printStringUtil("GAME OVER", 10, 20, 2, WHITE);
+       blinkWall = false;
+    }
+    else{
+       printStringUtil("GAME OVER", 10, 20, 2, BLACK);
+       blinkWall = true;
+    }
+    display.display();
+  }
+
   display.clearDisplay();
-  delay(3000);
+  printWinner();
+
+  player1Score = 0;
+  player2Score = 0;
+
+  ResetBall = true;
+  
+  
 }
